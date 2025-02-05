c8d26849fc5351c8484231e15df798b09494b512
rkipkazi1871C <rkipkazi1871@conestogac.on.ca>
2025-02-05 12:15:30 -0500

    Add project files.

---
+++ LibraryCatalog.cpp
@@ -0,0 +1,17 @@
+/*
+* FILE			: LibraryCatalog.cpp
+* PROJECT		: Data Structures Assignment 1
+* PROGRAMMERS	: NICHOLAS REILLY, RODRIGO GOMES, BRYAN LEONOR, RYAN KIPKAZI
+* FIRST VERSION	: 2025-02-01
+* DESCRIPTION	: The C++ file that will hold the source code for all of the functions used in the program.
+* REFERENCING   : Deitel, P., & Deitel, H. (2016). How to Program in C and C++ (8th ed.). Deitel & Associates Inc.
+*/
+
+//Reference to functions and other values. 
+#include "dataFunctions.h"
+
+//Main program.
+int main(void) {
+	mainMenu();
+	return 0;
+}
---
+++ dataFunctions.h
@@ -0,0 +1,47 @@
+/*
+* FILE			: dataFunctions.h
+* PROJECT		: Data Structures Assignment 1
+* PROGRAMMERS	: NICHOLAS REILLY, RODRIGO GOMES, BRYAN LEONOR, RYAN KIPKAZI
+* FIRST VERSION	: 2025-01-27
+* DESCRIPTION	: The header file that will hold defintions, declarations and function protoypes.
+* REFERENCING   : Deitel, P., & Deitel, H. (2016). How to Program in C and C++ (8th ed.). Deitel & Associates Inc.
+*/
+
+#pragma once
+
+//Referencing libraries used to porcess functions.
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+//Defining maximum array length definitions.
+#define TITLE_LENG 50
+#define AUTH_LENG 50
+
+//Defining the Book struct type.
+typedef struct {
+	int bookID;
+	char title[TITLE_LENG];
+	char author[AUTH_LENG];
+	int publicationYear;
+}Book;
+
+//Defining the BookNode struct type. I implemented this to traverse and create pointers for the books.
+typedef struct BookNode {
+	//The actual Book data entry.
+	Book data;
+	//The pointer to the next book.
+	struct BookNode* next;
+} BookNode;
+
+
+//Function prototypes.
+void displayMenu(void);
+void mainMenu(void);
+void addABook(BookNode** head);
+void searchABook(BookNode* head);
+int isDuplicateID(BookNode* head, int id);
+int titleContains(const char* title, const char* query);
+void toLowerCase(char* str);
+
